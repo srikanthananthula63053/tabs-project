1cbe3b205998fc99761be34b0f2f6e93b9dc2d39
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-10 16:36:44 +0530

    adding style

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TabTest from './components/tabs/tabs-test';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TabTest/>
+    </div>
+  );
+}
+
+export default App;
